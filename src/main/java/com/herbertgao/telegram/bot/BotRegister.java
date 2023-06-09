10bce66e10e15249ab5b2520f051c9b13b0c63af
HerbertGao <heapcn@gmail.com>
2023-06-09 13:58:47 +0800

    init: v6

--- src/main/java/com/herbertgao/telegram/bot/BotRegister.java
+++ src/main/java/com/herbertgao/telegram/bot/BotRegister.java
@@ -2,23 +2,28 @@ package com.herbertgao.telegram.bot;
 
 import com.herbertgao.telegram.bot.gaokao.GaokaoBot;
 import jakarta.annotation.PostConstruct;
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class BotRegister {
 
     @Autowired
     GaokaoBot gaokaoBot;
 
-    @SneakyThrows
     @PostConstruct
     public void init() {
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        telegramBotsApi.registerBot(gaokaoBot);
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(gaokaoBot);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
     }
 
 }
